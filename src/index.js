b5b78d9ec1fdb052d564b5e749a99ad1eb79b5c0
Ikemo4 <Isaac.morris@flybreeze.com>
2022-06-10 14:57:03 -0600

    Book image title and author components

--- src/index.js
+++ src/index.js
@@ -13,9 +13,18 @@ function BookList() {
 }
 
 const Book = () => {
-  return <article>
-    this is a nook
+  return(
+  <article>
+    <Image/>
+    <Title/>
+    <Author/>
   </article>
+  )
+};
+const Image = () => {
+  return <img src='https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL906_SR906,600_.jpg'></img>
 }
+const Title = () => <h1>If Animals Kissed Good Night</h1>
+const Author = () => <h4>Ann Whitford Paul</h4>
 
 ReactDom.render(<BookList/>,document.getElementById('root'));
