545e5a1af88ecd4e06f8a119be595e4d3a210c8b
Ikemo4 <Isaac.morris@flybreeze.com>
2022-06-13 14:28:15 -0600

    iterating simple list

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
 import ReactDom from 'react-dom';
 import './index.css'
 
-const firstBook = {
+const books = [
+  {
   img: 'https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL906_SR906,600_.jpg',
   title: 'If Animals Could Kiss Goodnight',
   author: 'Ann Whitford Paul'
-}
-
-const secondBook = {
+  },
+  {
   img: 'https://images-na.ssl-images-amazon.com/images/I/71eJk7GtUOL._AC_UL906_SR906,600_.jpg',
   title: 'The Boy, the Mole, the Fox and the Horse',
   author: 'Charlie Mackesy'
-}
+  }
+]
 
-
+const names = ['John', 'Peter', 'Susan']
+const newName = names.map((name) => {
+  return <h1>{name}</h1>
+});
 function BookList() {
   return (
   <section className='booklist'>
-    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
-      <p>
-        Lorem ipsum dolor
-      </p>
-    </Book>
-    <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
+    {newName}
   </section>
   );
 }
 
 //object destructuring javascript
-const Book = ({img, author, title, children}) => {
+const Book = ({img, author, title}) => {
 
   return(
   <article className='book'>
     <img src={img}></img>
     <h1>{title}</h1>
     <h4>{author}</h4>
-    {children}
   </article>
   )
 };
