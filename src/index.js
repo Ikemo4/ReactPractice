3710d56bd1b609c7c27cd22fb3de21b8c672601b
Ikemo4 <Isaac.morris@flybreeze.com>
2022-06-13 14:54:47 -0600

    key and object prop

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import './index.css'
 
 const books = [
   {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL906_SR906,600_.jpg',
-  title: 'If Animals Could Kiss Goodnight',
-  author: 'Ann Whitford Paul'
+    id: 1,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL906_SR906,600_.jpg',
+    title: 'If Animals Could Kiss Goodnight',
+    author: 'Ann Whitford Paul'
   },
   {
-  img: 'https://images-na.ssl-images-amazon.com/images/I/71eJk7GtUOL._AC_UL906_SR906,600_.jpg',
-  title: 'The Boy, the Mole, the Fox and the Horse',
-  author: 'Charlie Mackesy'
-  }
+    id: 2,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71eJk7GtUOL._AC_UL906_SR906,600_.jpg',
+    title: 'The Boy, the Mole, the Fox and the Horse',
+    author: 'Charlie Mackesy'
+  },
+  {
+    id: 3,
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81EVdWdmOKL._AC_UL906_SR906,600_.jpg',
+    title: 'Brown Bear, Brown Bear, What Do You See?',
+    author: 'Bill Martin Jr'
+  },
 ]
 
 function BookList() {
@@ -20,10 +28,7 @@ function BookList() {
     {books.map((book) => {
       const {img, title, author} = book;
       return(
-      <div>
-          <h2>{title}</h2>
-          <h4>{author}</h4>
-      </div>
+        <Book key={book.id} book={book}></Book>
       )
     })}
   </section>
@@ -31,8 +36,8 @@ function BookList() {
 }
 
 //object destructuring javascript
-const Book = ({img, author, title}) => {
-
+const Book = (props) => {
+  const {img, title, author} = props.book
   return(
   <article className='book'>
     <img src={img}></img>
