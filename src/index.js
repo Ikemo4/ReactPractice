9ed391833eeaca891724e17b29232e712a2f9aa2
Ikemo4 <Isaac.morris@flybreeze.com>
2022-06-13 15:16:05 -0600

    onClick and onHover

--- src/index.js
+++ src/index.js
@@ -39,12 +39,28 @@ function BookList() {
 
 //object destructuring javascript
 const Book = ({img, title, author}) => {
-
+  //attribute, event handler
+  //onClick, onMouseOver
+  const clickHandler = (event) => {
+    console.log(event);
+    alert('hello world');
+  };
+  const complexExample = (author) => {
+    console.log(author);
+  };
   return(
-  <article className='book'>
+  <article className='book' onMouseOver={() => {
+    console.log(title)
+  }}>
     <img src={img}></img>
-    <h1>{title}</h1>
+    <h1 onClick={()=> console.log(title)}>{title}</h1>
     <h4>{author}</h4>
+    <button type='button' onClick={clickHandler}>
+      reference example
+    </button>
+    <button type='button' onClick={() => complexExample(author)}> {/* can't just do complexExample(author) because that calls the function immediately */}
+      more complex example
+    </button>
   </article>
   )
 };
