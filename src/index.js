c995e9ad33dae3297dce81d701c2f59d0cbe2955
Ikemo4 <Isaac.morris@flybreeze.com>
2022-06-13 13:58:16 -0600

    props children

--- src/index.js
+++ src/index.js
@@ -17,41 +17,27 @@ const secondBook = {
 function BookList() {
   return (
   <section className='booklist'>
-    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+    <Book img={firstBook.img} title={firstBook.title} author={firstBook.author}>
+      <p>
+        Lorem ipsum dolor
+      </p>
+    </Book>
     <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
   </section>
   );
 }
 
 //object destructuring javascript
-const Book = (props) => {
-  const {img, title, author} = props
+const Book = ({img, author, title, children}) => {
+
   return(
   <article className='book'>
     <img src={img}></img>
     <h1>{title}</h1>
     <h4>{author}</h4>
+    {children}
   </article>
   )
 };
-// const Book = ({img, title, author}) => {
-//   return(
-//   <article className='book'>
-//     <img src={img}></img>
-//     <h1>{title}</h1>
-//     <h4>{author}</h4>
-//   </article>
-//   )
-// };
-
-// const Book = (props) => {
-//   return(
-//   <article className='book'>
-//     <img src={props.img}></img>
-//     <h1>{props.title}</h1>
-//     <h4>{props.author}</h4>
-//   </article>
-//   )
-// };
 
 ReactDom.render(<BookList/>,document.getElementById('root'));
